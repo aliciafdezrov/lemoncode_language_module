3c309d7a5174aa8f799b67ce16e6ff0e79e6cb02
alicia <aliciafdezrov@gmail.com>
2019-11-22 14:16:44 +0100

    Completed third section of exercise 5: memoization

--- src/advanced/memoizacion.ts
+++ src/advanced/memoizacion.ts
@@ -12,7 +12,7 @@ const memoize = (func: () => any) => {
     let funcKey = {name: func.name};
 
     return () => {
-        if(cache.get(funcKey) !== undefined) {
+        if (cache.get(funcKey) !== undefined) {
             return cache.get(funcKey);
         } else {
             let funcValue = func();
@@ -34,20 +34,20 @@ interface FuncType {
 }
 
 const cacheAPI = (func: () => any) => {
-  const cache = new WeakMap();
-  const key: FuncType = {name};
+    const cache = new WeakMap();
+    const key: FuncType = {name};
 
-  return {
-      setCache: function () {
-          let value = func();
-          cache.set(key, value);
-          return value;
-      },
+    return {
+        setCache: function () {
+            let value = func();
+            cache.set(key, value);
+            return value;
+        },
 
-      getCacheItem: function () {
-          return cache.get(key);
-      }
-  }
+        getCacheItem: function () {
+            return cache.get(key);
+        }
+    }
 };
 
 const memoizeInOneLine = (func: () => any, cache = cacheAPI(func)) => () => cache.getCacheItem() !== undefined ? cache.getCacheItem() : cache.setCache();
@@ -56,3 +56,89 @@ const memoizedInOneLine = memoizeInOneLine(expensiveFunction);
 console.log(memoizedInOneLine());
 console.log(memoizedInOneLine());
 console.log(memoizedInOneLine());
+
+console.log("APARTADO C");
+
+let count = 0; // Comprobacion de no de ejecuciones
+const repeatText = (repetitions: number, text: string): string => (count++, `${text} `.repeat(repetitions).trim());
+
+interface FuncTypeWithArguments {
+    name: string;
+    arguments: Array<any>;
+}
+
+const keyAPI = () => {
+    const key: FuncTypeWithArguments = {
+        name: null,
+        arguments: null
+    };
+
+    return {
+        getKey: function () {
+            return key;
+        },
+
+        setKeyName: function (name: string) {
+            key.name = name;
+        },
+
+        setKeyArgs: function (args: Array<any>) {
+            key.arguments = args;
+        }
+    }
+};
+
+const cacheAPI2 = (func) => {
+    const cache = new Map();
+
+    return {
+        setCache: function (key) {
+            let value = func(...key.getKey().arguments);
+            cache.set(key.getKey(), value);
+            return value;
+        },
+
+        getItemFromCache: function(key) {
+          return cache.get(key);
+        },
+
+        hasCacheThisItem: function (key) {
+            let cacheItem = null;
+
+            if (cache.size) {
+                cache.forEach((value, key1) => {
+                    if (key1.name === key.getKey().name && key.getKey().arguments.length === key1.arguments.length) {
+                        let elementsAreEqual = true;
+                        for(let i = 0; i < key1.arguments.length && elementsAreEqual; i++) {
+                            if (key.getKey().arguments[i] !== key1.arguments[i]) elementsAreEqual = false;
+                        }
+
+                        if(elementsAreEqual) cacheItem = key1;
+                    }
+                });
+            }
+            return cacheItem;
+        }
+    }
+};
+
+const memoizeWithArgs = (func) => {
+    const cache = cacheAPI2(func);
+    const keys = Array<FuncTypeWithArguments>();
+
+    return (...args) => {
+        const key = keyAPI();
+        key.setKeyName(func.name);
+        key.setKeyArgs(args);
+        let itemFromCache = cache.hasCacheThisItem(key);
+        return itemFromCache !== null ? cache.getItemFromCache(itemFromCache) : cache.setCache(key);
+    };
+};
+
+const memoizedGreet = memoizeWithArgs(repeatText);
+console.log(memoizedGreet(1, "pam"));   // pam
+console.log(memoizedGreet(3, "chun"));  // chun chun chun
+console.log(memoizedGreet(1, "pam"));   // pam
+console.log(memoizedGreet(3, "chun"));  // chun chun chun
+console.log(count);                     // 2
+
