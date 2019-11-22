46bfcc6b20aa1f0b9344b9b25d4bc6f3023e6384
alicia <aliciafdezrov@gmail.com>
2019-11-22 10:37:32 +0100

    Finished asynchronous exercise about console logs

---
+++ src/advanced/console_logs.ts
@@ -0,0 +1,21 @@
+console.log("----> 4. TRAZAS POR CONSOLA");
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const showMessage = async ([time, message]) => {
+    await delay(time);
+    console.log(message);
+};
+const triggers = [
+    async () => await showMessage([200, "third"]),
+    async () => await showMessage([100, "second"])
+];
+const run = triggers => {
+    triggers.forEach(async (t, i) => {
+        if (i === triggers.length - 1) {
+            await delay(100);
+            triggers[i]();
+            await showMessage([100, "first"]);
+        } else triggers[i]();
+    });
+};
+run(triggers);
