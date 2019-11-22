b84676e501b8713a5099a02fe7c2744b971759e3
alicia <aliciafdezrov@gmail.com>
2019-11-22 11:45:55 +0100

    Completed second section of exercise 5: memoization

--- src/advanced/memoizacion.ts
+++ src/advanced/memoizacion.ts
@@ -26,3 +26,33 @@ const memoized = memoize(expensiveFunction);
 console.log(memoized()); // Una única llamada // 3.1415
 console.log(memoized()); // 3.1415
 console.log(memoized()); // 3.1415
+
+console.log("APARTADO B: ");
+
+interface FuncType {
+    name: string;
+}
+
+const cacheAPI = (func: () => any) => {
+  const cache = new WeakMap();
+  const key: FuncType = {name};
+
+  return {
+      setCache: function () {
+          let value = func();
+          cache.set(key, value);
+          return value;
+      },
+
+      getCacheItem: function () {
+          return cache.get(key);
+      }
+  }
+};
+
+const memoizeInOneLine = (func: () => any, cache = cacheAPI(func)) => () => cache.getCacheItem() !== undefined ? cache.getCacheItem() : cache.setCache();
+
+const memoizedInOneLine = memoizeInOneLine(expensiveFunction);
+console.log(memoizedInOneLine());
+console.log(memoizedInOneLine());
+console.log(memoizedInOneLine());
